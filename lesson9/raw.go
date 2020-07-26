1578620da9a28cd7bc6d1465d6ba6fcb7c2df75c
William Munoz <william.munozr@gmail.com>
2020-07-25 21:01:00 -0500

    Lesson 9. Multilingual text

---
+++ lesson9/raw.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Peace be upon you\nupon you be peace.")
+	fmt.Println(`strings can span multiple 
+	lines with the \n escape 
+	sequence`)
+}
